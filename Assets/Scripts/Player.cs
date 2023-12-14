0f9c750301674c721680e324a31c750902d9fef5
Bahadır Işık <76049821+bahadirisik@users.noreply.github.com>
2023-12-14 12:23:44 +0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,20 +11,15 @@ public class Player : MonoBehaviour
     private Vector3 firstPosition;
     private Vector3 targetPosition;
 
-    //private Transform target;
-
     private float timeToMove;
     private float startTimeToMove = 3f;
-    //private int currentHealt;
 
     private Rigidbody2D rb;
 
     private bool isArrived = false;
-    //private bool isAttacking = false;
 
     void Start()
     {
-        //currentHealt = playerSO.playerHealth;
         rb = GetComponent<Rigidbody2D>();
         firstPosition = transform.position;
         timeToMove = startTimeToMove;
@@ -34,19 +29,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (currentHealt <= 0)
-        {
-            Death();
-        }*/
-
-        //FindClosestTarget();
-
-
-        /*if (GetDistance(target) < playerSO.minDistanceToAttack && !isAttacking)
-        {
-            StartCoroutine(AttackTheClosestTarget());
-        }*/
-
         if (timeToMove < 0f)
         {
             timeToMove = startTimeToMove;
@@ -91,62 +73,8 @@ public class Player : MonoBehaviour
         return newRandomPosition;
     }
 
-    /*void FindClosestTarget()
-    {
-        float minDistance = Mathf.Infinity;
-        foreach (Transform enemy in EnemySpawnManager.Instance.GetEnemyList())
-        {
-            if (GetDistance(enemy) < minDistance)
-            {
-                target = enemy;
-                minDistance = Vector3.Distance(enemy.position, transform.position);
-            }
-        }
-    }*/
-
-    /*IEnumerator AttackTheClosestTarget()
-    {
-        isAttacking = true;
-        if (GetDistance(target) < playerSO.minDistanceToDamage)
-        {
-            target.GetComponent<Enemy>().DecreaseHealth(playerSO.playerDamage);
-        }
-
-        yield return new WaitForSeconds(playerSO.playerFireRate);
-
-        isAttacking = false;
-        StopCoroutine(AttackTheClosestTarget());
-    }*/
-
-    /*private float GetDistance(Transform target)
-    {
-        if (target != null)
-        {
-            return Vector3.Distance(transform.position, target.position);
-        }
-
-        return Mathf.Infinity;
-    }*/
-
-    /*private void Death()
-    {
-        SpawnManager.Instance.RemovePlayerFromList(transform);
-
-        Destroy(gameObject);
-    }*/
-
-    /*public void DecreaseHealth(int damage)
-    {
-        currentHealt -= damage;
-    }*/
-
     public PlayerSO GetPlayerSO()
 	{
         return playerSO;
 	}
-
-	/*public void DecreaseHealth(int damage)
-	{
-        currentHealt -= damage;
-	}*/
 }
